57a4fb26078014b29caf64e37b3e58b80a6acba2
Vinicius <vinicius.unisinos@hotmail.com>
2019-05-03 22:18:58 -0300

    Alteração de BASE_URL para BASE no login.

--- views/login.php
+++ views/login.php
@@ -2,7 +2,7 @@
     <head>
         <meta charset="UTF-8">
         <title>Login</title>
-        <link href="<?php echo BASE_URL; ?>/assets/css/login.css" rel="stylesheet" />
+        <link href="<?php echo BASE; ?>/assets/css/login.css" rel="stylesheet" />
     </head>
     <body>
         <div class="loginarea">
